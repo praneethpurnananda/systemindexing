3e89859dfeff802ff854b98670060c7b638918e0
Purnananda Praneeth <praneeth.mandalemula@gmail.com>
2020-02-18 18:46:04 +0530

    systemindexing

---
+++ assets/dashscript.js
@@ -0,0 +1,34 @@
+$(document).ready( function() {
+
+  $('body').on("click", ".larg div h3", function(){
+    if ($(this).children('span').hasClass('close')) {
+      $(this).children('span').removeClass('close');
+    }
+    else {
+      $(this).children('span').addClass('close');
+    }
+    $(this).parent().children('p').slideToggle(250);
+  }
+
+/*mycode*
+
+$('body').on("click", ".larg div h3", function(){
+  if ($(this).children('span').hasClass('close')) {
+    $(this).children('span').removeClass('close');
+  }
+  else {
+    $(this).children('span').addClass('close');
+  }
+  $(this).parent().children('p').slideToggle(250);
+}
+
+/*mycode*/
+
+
+);
+
+  $('body').on("click", "nav ul li a", function(){
+    var title = $(this).data('title');
+    $('.title').children('h2').html(title);
+  });
+});
